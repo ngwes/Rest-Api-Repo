1602e73245ba5095064b8e5b329bb790be724068
nadir gherbi <nadghe@live.com>
2021-09-13 20:43:07 +0200

    Add api versioning

---
+++ Rest Api Repo/Controllers/V1/PostsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rest_Api_Repo.Contracts;
+using Rest_Api_Repo.Contracts.V1;
+using Rest_Api_Repo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest_Api_Repo.Controllers.V1
+{
+    public class PostsController : Controller
+    {
+        private List<Post> _posts;
+
+        public PostsController()
+        {
+            _posts = new List<Post>();
+            for (int i = 0; i < 5; i++)
+            {
+                _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
+            }
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(_posts);
+        }
+    }
+}
