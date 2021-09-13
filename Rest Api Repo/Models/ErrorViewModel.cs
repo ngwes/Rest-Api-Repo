3510fe3e3d1e31b80490f0a70fcad182ae4b9ab4
nadir gherbi <nadghe@live.com>
2021-09-13 19:37:21 +0200

    Add project files.

---
+++ Rest Api Repo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rest_Api_Repo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
